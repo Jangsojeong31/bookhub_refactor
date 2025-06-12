63f89e0cc8b86645f51eda9ecadc8e1585b168fc
Jangsojeong31 <sojung301@naver.com>
2025-06-12 16:00:09 +0900

    feat: purchaseOrder jsj

---
+++ src/apis/purchaseOrder/purchaseOrder.ts
@@ -0,0 +1,50 @@
+import { ResponseDto } from "@/dtos";
+import { PurchaseOrderCreateRequestDto } from "@/dtos/purchaseOrder/request/purchaseOrder-create.request.dto";
+import { PurchaseOrderResponseDto } from "@/dtos/purchaseOrder/response/purchaseOrder.response.dto";
+import { axiosInstance, bearerAuthorization, responseErrorHandler, responseSuccessHandler } from "../axiosConfig";
+import { DELETE_PURCHASE_ORDER_URL, GET_ALL_PURCHASE_ORDER_URL, POST_PURCHASE_ORDER_URL, PUT_PURCHASE_ORDER_URL } from "../constants/jsj.constants";
+import { AxiosError } from "axios";
+import { PurchaseOrderRequestDto } from "@/dtos/purchaseOrder/request/purchaseOrder.request.dto";
+
+
+// 발주 요청서 작성 (사용자 정보 가져와서 이름, 지점 등 사용)
+export const createPurchaseOrder = async(dto: PurchaseOrderCreateRequestDto, accessToken: string): Promise<ResponseDto<PurchaseOrderResponseDto[]>> => {
+  try{
+    const response = await axiosInstance.post(POST_PURCHASE_ORDER_URL, dto, bearerAuthorization(accessToken));
+    return responseSuccessHandler(response);
+  }catch(error){
+    return responseErrorHandler(error as AxiosError<ResponseDto>);
+
+  }
+}
+
+// 발주 요청서 전체 조회
+export const getAllPurchaseOrder = async(): Promise<ResponseDto<PurchaseOrderResponseDto[]>> => {
+  try{
+    const response = await axiosInstance.get(GET_ALL_PURCHASE_ORDER_URL);
+    return responseSuccessHandler(response);
+  }catch(error){
+    return responseErrorHandler(error as AxiosError<ResponseDto>)
+  }
+}
+// 단건 조회(id)
+// 조회 기준으로 조회(발주 담당 사원, isbn, 승인 상태)
+// 수정(발주량 수정)
+export const updatePurchaseOrder = async(purchaseOrderId: number, dto: PurchaseOrderRequestDto): Promise<ResponseDto<PurchaseOrderResponseDto>> => {
+  try{
+    const response = await axiosInstance.put(PUT_PURCHASE_ORDER_URL(purchaseOrderId), dto);
+    return responseSuccessHandler(response);
+  }catch(error){
+    return responseErrorHandler(error as AxiosError<ResponseDto>)
+  }
+}
+
+// 삭제
+export const deletePurchaseOrder = async(purchaseOrderId: number): Promise<ResponseDto<void>> => {
+  try{
+    const response = await axiosInstance.delete(DELETE_PURCHASE_ORDER_URL(purchaseOrderId));
+    return responseSuccessHandler(response);
+  }catch(error){
+    return responseErrorHandler(error as AxiosError<ResponseDto>)
+  }
+}
