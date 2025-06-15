3147c419697258610c1d97b9876f57f4515daeef
kohyeokjae <kohj0686@naver.com>
2025-06-15 22:57:07 +0900

    feat: password-change khj

--- src/views/auth/PasswordChangeSendEmail.tsx
+++ src/views/auth/PasswordChangeSendEmail.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { passwordChangeEmailRequest } from "@/apis/auth/auth";
+import { PasswordChangeEamilRequestDto } from "@/dtos/auth/request/password-change-email.request.dto";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function PasswordChangeSendEmail() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    loginId: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    setMessage("");
+    return;
+  }, [form]);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const onEmailSendClick = async () => {
+    const { loginId, email, phoneNumber } = form;
+
+    if (!loginId || !email || !phoneNumber) {
+      setMessage("모든 항목을 입력해주세요.");
+      return;
+    }
+
+    const responseBody: PasswordChangeEamilRequestDto = {
+      loginId,
+      email,
+      phoneNumber,
+    };
+    const response = await passwordChangeEmailRequest(responseBody);
+    const { code, message, data } = response;
+
+    if (code != "SU") {
+      setMessage("이메일 전송 실패: " + message);
+      return;
+    } else {
+      alert(data);
+      navigate("/auth/login");
+    }
+  };
+
   return (
-    <div>PasswordChangeSendEmail</div>
-  )
+    <div>
+      <h1>비밀번호 변경</h1>
+      <input
+        type="text"
+        placeholder="아이디"
+        name="loginId"
+        value={form.loginId}
+        onChange={onInputChange}
+      />
+      <br />
+      <input
+        type="email"
+        placeholder="이메일"
+        name="email"
+        value={form.email}
+        onChange={onInputChange}
+      />
+      <br />
+      <input
+        type="tel"
+        placeholder="전화번호"
+        name="phoneNumber"
+        value={form.phoneNumber}
+        onChange={onInputChange}
+      />
+      <br />
+      <button onClick={onEmailSendClick}>이메일 전송</button>
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
 
-export default PasswordChangeSendEmail
+export default PasswordChangeSendEmail;
--- src/views/auth/passwordChange.tsx
+++ src/views/auth/passwordChange.tsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import { passwordChangeRequest, verifyToken } from "@/apis/auth/auth";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
-function PasswordChange() {
+const PasswordChange = () => {
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token") || "";
+
+  const [loading, setLoading] = useState(true);
+  const [verified, setVerified] = useState(false);
+
+  const [form, setForm] = useState({ password: "", confirmPassword: "" });
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchPasswordChange = async () => {
+      if (!token) {
+        setMessage("유효하지 않은 접근입니다. 토큰이 없습니다.");
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await verifyToken(token);
+        const { code, message } = response;
+
+        if (code == "SU") {
+          setVerified(true);
+          return;
+        } else {
+          setMessage(message);
+          return;
+        }
+      } catch (error) {
+        setMessage("서버 오류가 발생했습니다.");
+        return;
+      } finally {
+        setLoading(false);
+        return;
+      }
+    };
+    fetchPasswordChange();
+    setMessage("");
+  }, [token]);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const onButtonClick = async () => {
+    const { password, confirmPassword } = form;
+    if (!password || !confirmPassword) {
+      setMessage("모든 항목을 입력하세요");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setMessage("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    try {
+      const response = await passwordChangeRequest(token, {
+        password,
+        confirmPassword,
+      });
+
+      const { code, message } = response;
+
+      if (code == "SU") {
+        alert("비밀번호가 성공적으로 변경되었습니다. 창을 닫습니다.");
+        window.close();
+        if (!closed) {
+          alert(
+            "창이 자동으로 닫히지 않으면 브라우저 탭을 수동으로 닫아주세요."
+          );
+        }
+      } else {
+        setMessage(message || "비밀번호 변경에 실패했습니다.");
+      }
+    } catch {
+      setMessage("오류가 발생했습니다.");
+    }
+  };
+
   return (
-    <div>PasswordChange</div>
-  )
-}
+    <div>
+      {!loading && verified && (
+        <>
+          <h2>비밀번호 변경</h2>
+          <input
+            type="password"
+            placeholder="새 비밀번호"
+            name="password"
+            value={form.password}
+            onChange={onInputChange}
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="새 비밀번호 확인"
+            name="confirmPassword"
+            value={form.confirmPassword}
+            onChange={onInputChange}
+            required
+          />
+          <br />
+          <button onClick={onButtonClick}>비밀번호 변경</button>
+          {message && <p>{message}</p>}
+        </>
+      )}
+      {!loading && !verified && <p>{message}</p>}
+    </div>
+  );
+};
 
-export default PasswordChange
+export default PasswordChange;
