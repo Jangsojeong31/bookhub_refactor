afc353d93dce3c2c5a7b920627ee6202b553a5e7
kohyeokjae <kohj0686@naver.com>
2025-06-17 12:23:40 +0900

    feat: employeeDetail khj

--- src/apis/employee/employee.ts
+++ src/apis/employee/employee.ts
@@ -14,6 +14,8 @@ import {
 } from "../constants/khj.constants";
 import { AxiosError } from "axios";
 import { EmployeeSignUpApprovalRequestDto } from "@/dtos/employee/request/employee-sign-up-Approval.request.dto";
+import { EmployeeDetailResponseDto } from "@/dtos/employee/response/employee-detail.response.dto";
+import { access } from "fs";
 
 interface SearchEmployeeParams {
   name?: string;
@@ -32,6 +34,18 @@ export const employeeResquest = async (
       params,
       ...bearerAuthorization(accessToken),
     });
+    return responseSuccessHandler(response);
+  } catch (error) {
+    return responseErrorHandler(error as AxiosError<ResponseDto>);
+  }
+};
+
+export const employeeDetailResquest = async (
+  employeeId: number,
+  accessToken: string
+): Promise<ResponseDto<EmployeeDetailResponseDto>> => {
+  try {
+    const response = await axiosInstance.get(GET_EMPLOYEE_URL(employeeId), bearerAuthorization(accessToken));
     return responseSuccessHandler(response);
   } catch (error) {
     return responseErrorHandler(error as AxiosError<ResponseDto>);
