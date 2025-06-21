e4c2254ed9c82bcc1cd684527065b9e63f330f72
kohyeokjae <kohj0686@naver.com>
2025-06-21 21:17:57 +0900

    feat: statistics khj

--- src/apis/statistics/stocksStatistics/stocksStatistics.ts
+++ src/apis/statistics/stocksStatistics/stocksStatistics.ts
@@ -4,22 +4,45 @@ import {
   responseErrorHandler,
   responseSuccessHandler,
 } from "@/apis/axiosConfig";
-import { STOCK_STATISTICS_BRANCH_URL } from "@/apis/constants/csy.constants";
+import {
+  STOCK_STATISTICS_BRANCH_URL,
+  STOCK_STATISTICS_CATEGORY_URL,
+} from "@/apis/constants/csy.constants";
 import { ResponseDto } from "@/dtos";
 import { BranchStockBarChartResponseDto } from "@/dtos/statistics/StocksStatistics/response/branchStockBarChart.response.dto";
+import { CategoryStockResponseDto } from "@/dtos/statistics/StocksStatistics/response/categoryStock.response.dto";
 import { AxiosError } from "axios";
 
-interface searchParams {
+interface searchDateParams {
   year: number;
   month: number;
 }
 
+interface searchBranchParams {
+  branchName: string;
+}
+
 export const branchStockBarChartRequest = async (
-  params: searchParams,
+  params: searchDateParams,
   accessToken: string
 ): Promise<ResponseDto<BranchStockBarChartResponseDto[]>> => {
   try {
     const response = await axiosInstance.get(STOCK_STATISTICS_BRANCH_URL, {
+      params,
+      ...bearerAuthorization(accessToken),
+    });
+    return responseSuccessHandler(response);
+  } catch (error) {
+    return responseErrorHandler(error as AxiosError<ResponseDto>);
+  }
+};
+
+export const categoryStockRequest = async (
+  params: searchBranchParams,
+  accessToken: string
+): Promise<ResponseDto<CategoryStockResponseDto[]>> => {
+  try {
+    const response = await axiosInstance.get(STOCK_STATISTICS_CATEGORY_URL, {
       params,
       ...bearerAuthorization(accessToken),
     });
