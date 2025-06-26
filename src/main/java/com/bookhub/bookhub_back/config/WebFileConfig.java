e6233729f054daed39972b331a1cdaa1692bd220
Bottomdeal <zpalqm1357@nate.com>
2025-06-26 10:57:26 +0900

    refactor: file sjw

--- src/main/java/com/bookhub/bookhub_back/config/WebFileConfig.java
+++ src/main/java/com/bookhub/bookhub_back/config/WebFileConfig.java
@@ -1,14 +1,17 @@
 package com.bookhub.bookhub_back.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebFileConfig implements WebMvcConfigurer {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
-                .addResourceLocations("file:C:/upload/file/");
+                .addResourceLocations("file:" + uploadDir + "/");
     }
 }
